1cf30511294b488e9f60e7f8ab3fd6da10bedd18
Mehdi Dehghani <dehghani.m.c@gmail.com>
2018-10-11 12:55:19 +0330

    Added * char to emmetSpec chars

--- Emmet/EditorExtensions/EmmetCommandTarget.cs
+++ Emmet/EditorExtensions/EmmetCommandTarget.cs
@@ -16,7 +16,7 @@ namespace Emmet.EditorExtensions
     public class EmmetCommandTarget : CommandTargetBase
     {
         private List<string> _htmlTags = new List<string>() { "link", "meta", "style", "title", "html", "body", "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "dir", "div", "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre", "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn", "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "small", "span", "strong", "sub", "sup", "time", "tt", "u", "var", "wbr", "area", "audio", "img", "map", "track", "video", "applet", "embed", "iframe", "noembed", "object", "param", "picture", "source", "canvas", "noscript", "script", "del", "ins", "caption", "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "button", "datalist", "fieldset", "form", "input", "label", "legend", "meter", "optgroup", "option", "output", "progress", "select", "textarea", "details", "dialog", "menu", "menuitem", "summary", "content", "element", "shadow", "slot", "template" };
-        private List<string> _emmetSpecChars = new List<string>() { ".", "#", ">", "{", "[" };
+        private List<string> _emmetSpecChars = new List<string>() { ".", "#", ">", "{", "[", "*" };
 
         private readonly ICompletionBroker _completionBroker;
 
